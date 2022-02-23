dcea03e6170a2c0e2ca2c16273ae1a35a7f3c397
Caio de la Rosa <caio.gomiero@hotmail.com>
2022-02-23 20:27:45 -0300

    Teste do teste

--- TestesAulas/Program.cs
+++ TestesAulas/Program.cs
@@ -1,13 +1,1 @@
-﻿int ano = 0;
-int pa = 100;
-double pb = 200;
-
-while (pa < pb)
-{
-    pa = pa + (pa * 10 / 100);
-    pb = pb + (pb * 1.5 / 100);
-    ano = ano + 1;
-
-}
-
-Console.WriteLine(ano);
+﻿Console.WriteLine("Teste do teste");
